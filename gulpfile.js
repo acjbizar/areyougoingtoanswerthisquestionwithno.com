f16a7df4b45a9b0e4941d141eb72089204f4ec99
ACJ <gitlab@acjs.net>
2022-02-14 20:33:14 +0100

    Gulp Sass demo

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./src/css'));
+}
+
+exports.buildStyles = buildStyles;
+exports.watch = function () {
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+};
